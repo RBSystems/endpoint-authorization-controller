21cae92301e60175a95c994348630512544c5476
Joseph Blodgett <joseph.blodgett@gmail.com>
2018-10-29 08:48:39 -0600

    couchauthdb interface fulfilment done

--- db/couch/couchauthdb.go
+++ db/couch/couchauthdb.go
@@ -14,7 +14,11 @@ func GetDB() *AuthDB {
 	return &AuthDB{}
 }
 
-//GetPermissionRecords .
+//GetPermissionRecords retrieves all the relevant permission structures for the ID requested.
+//if reqID doesn't correspond to a 'leaf' resource, all sub-resources will be included.
+//if reqID does correpond to a 'leaf' node, ALL non leaf resources will have a record included. If one is not defined in the database, one will be generated with blank permissions.
+//The leaf record itself MAY be omitted if not explicitly defined in the permissions database - but it's reccommneded that it have a blank record generated as well.
 func (a *AuthDB) GetPermissionRecords(reqType, reqID string) (map[string]base.PermissionsRecord, *nerr.E) {
+
 	return map[string]base.PermissionsRecord{}, nil
 }
