b128a8bf239d1749987103caf838321b0c6e0cec
CalebLarsen <caleb.john.larsen@gmail.com>
2018-11-19 14:18:00 -0700

    Fixed authorization bug

--- permissions/permissions.go
+++ permissions/permissions.go
@@ -16,8 +16,8 @@ import (
 
 // Authentication method constants
 const (
-	WSO2            = "wso2"
-	CAS             = "cas"
+	WSO2            = "WSO2"
+	CAS             = "CAS"
 	Application     = "APPLICATION"
 	ApplicationUser = "APPLICATION_USER"
 )
@@ -72,12 +72,13 @@ const (
 //GetAuthorization .
 func GetAuthorization(req base.Request) (base.Response, *nerr.E) {
 	var groups []string
-
+	var ok bool
+	var ne *nerr.E
 	log.L.Infof("getting permissions for %s with %s", req.UserInformation.ID, req.UserInformation.ResourceID)
 
 	switch req.UserInformation.AuthMethod {
 	case WSO2:
-		ok, ne := getWSO2Authorization(req, &groups)
+		ok, groups, ne = getWSO2Authorization(req)
 		if ne != nil {
 			log.L.Errorf("something went wrong : %s", ne.String())
 			return base.Response{}, nil
@@ -87,7 +88,8 @@ func GetAuthorization(req base.Request) (base.Response, *nerr.E) {
 			return base.Response{}, nil
 		}
 	case CAS:
-		ok, ne := getCASAuthorization(req, groups)
+		ok, groups, ne = getCASAuthorization(req)
+		log.L.Debugf("Groups 2: %s - Should be the same", groups)
 		if ne != nil {
 			log.L.Errorf("something went wrong : %s", ne.String())
 			return base.Response{}, nil
@@ -112,20 +114,22 @@ func GetAuthorization(req base.Request) (base.Response, *nerr.E) {
 		}
 		return toReturn, nil
 	}
-	log.L.Debugf("%v", toReturn.ID)
+	log.L.Debugf("ID: %v", toReturn.ID)
 
 	return toReturn, nil
 }
 
-func getCASAuthorization(req base.Request, groups []string) (bool, *nerr.E) {
+func getCASAuthorization(req base.Request) (bool, []string, *nerr.E) {
 	//check the API key
+	var groups []string
+	log.L.Debug("Getting Key record for CAS")
 	keyrec, err := db.GetAuthDB(couch.COUCH).GetKeyRecord(req.AccessKey)
 	if err != nil {
-		return false, err.Addf("Couldn't authorize api key")
+		return false, groups, err.Addf("Couldn't authorize api key")
 	}
 
 	if !keyrec.Valid {
-		return false, nerr.Create(fmt.Sprintf("API key is invalid"), "invalid-key")
+		return false, groups, nerr.Create(fmt.Sprintf("API key is invalid"), "invalid-key")
 	}
 
 	log.L.Infof("userID: %v", req.UserInformation.ID)
@@ -134,28 +138,30 @@ func getCASAuthorization(req base.Request, groups []string) (bool, *nerr.E) {
 			//we get the permissions based on the gruops defined on the service
 			groups = keyrec.Groups
 		} else {
-			return false, nerr.Create(fmt.Sprintf("API Key not valid for service user."), "bad-user")
+			return false, groups, nerr.Create(fmt.Sprintf("API Key not valid for service user."), "bad-user")
 		}
 	} else {
 		//we need to get the groups for the user from AD
 		groups, err = users.GetGroupsForUser(req.UserInformation.ID, users.LDAP)
 		if err != nil {
-			return false, err.Addf("couldn't generate authorizations")
+			return false, groups, err.Addf("couldn't generate authorizations")
 		}
 	}
-
-	return true, nil
+	log.L.Debugf("Groups: %s", groups)
+	return true, groups, nil
 }
 
-func getWSO2Authorization(req base.Request, groups *[]string) (bool, *nerr.E) {
+func getWSO2Authorization(req base.Request) (bool, []string, *nerr.E) {
 	// check if the token is valid
+	var groups []string
+
 	ok, token, err := Validate(string(req.UserInformation.Data))
 	if err != nil {
-		return false, nerr.Translate(err).Addf("JWT token is invalid")
+		return false, groups, nerr.Translate(err).Addf("JWT token is invalid")
 	}
 
 	if !ok {
-		return false, nil
+		return false, groups, nil
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
@@ -170,11 +176,11 @@ func getWSO2Authorization(req base.Request, groups *[]string) (bool, *nerr.E) {
 	//check the API key
 	keyrec, ne := db.GetAuthDB(couch.COUCH).GetKeyRecord(req.AccessKey)
 	if err != nil {
-		return false, ne.Addf("Couldn't authorize api key")
+		return false, groups, ne.Addf("Couldn't authorize api key")
 	}
 
 	if !keyrec.Valid || !keyrec.HasAuthMethod(req.UserInformation.AuthMethod) {
-		return false, nerr.Create(fmt.Sprintf("API key is invalid"), "invalid-key")
+		return false, groups, nerr.Create(fmt.Sprintf("API key is invalid"), "invalid-key")
 	}
 
 	// get the usertype
@@ -183,19 +189,19 @@ func getWSO2Authorization(req base.Request, groups *[]string) (bool, *nerr.E) {
 	if usertype == Application {
 		if keyrec.Service {
 			//we get the permissions based on the gruops defined on the service
-			*groups = keyrec.Groups
+			groups = keyrec.Groups
 		} else {
-			return false, nerr.Create(fmt.Sprintf("API Key not valid for service user."), "bad-user")
+			return false, groups, nerr.Create(fmt.Sprintf("API Key not valid for service user."), "bad-user")
 		}
 	} else if usertype == ApplicationUser {
 		req.UserInformation.ID = claims["http://byu.edu/claims/resourceowner_net_id"].(string)
 
-		*groups, ne = users.GetGroupsForUser(req.UserInformation.ID, users.LDAP)
+		groups, ne = users.GetGroupsForUser(req.UserInformation.ID, users.LDAP)
 		if ne != nil {
-			return false, ne.Addf("Couldn't generate authorizations")
+			return false, groups, ne.Addf("Couldn't generate authorizations")
 		}
 
 	}
 
-	return true, nil
+	return true, groups, nil
 }
